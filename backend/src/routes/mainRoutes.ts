6552df7999d63924efe3db439f3a13d9c40804af
leonardo-policarpo <leo.dumont@outlook.com>
2025-09-01 23:56:32 -0300

    feat: implement server and main routes

---
+++ backend/src/routes/mainRoutes.ts
@@ -0,0 +1,12 @@
+import { FastifyPluginAsyncZod } from 'fastify-type-provider-zod';
+
+const mainRoutes: FastifyPluginAsyncZod = async app => {
+  app.get('/', async (request, reply) => {
+    reply.status(200).send({ message: "Hello from IOT-Firmware-Update API"})
+  });
+  app.get('/health', async (request, reply) => {
+    reply.status(200).send({ message: "Route to check api health"})
+  })
+};
+
+export default mainRoutes;
